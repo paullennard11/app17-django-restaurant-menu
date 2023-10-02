766be5197613e40993e88cf37bd913b36f861a7c
Lennard <lennardpolito@gmail.com>
2023-10-02 19:41:03 +0800

    add Jinja If Conditionals, Add Dynamic Links, and Add Bootstrap

--- qr.py
+++ qr.py
@@ -1,5 +1,5 @@
 import qrcode
 
-image = qrcode.make("http://127.0.0.1:8000")
+image = qrcode.make("http://127.0.0.1:8005")
 image.save("qr.png")
 
--- restaurant_menu/models.py
+++ restaurant_menu/models.py
@@ -11,7 +11,7 @@ MEAL_TYPE = (
 
 STATUS = (
     (0, "Unavailable"),
-    (0, "Available")
+    (1, "Available")
 )
 
 
@@ -21,7 +21,7 @@ class Item(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     meal_type = models.CharField(max_length=200, choices=MEAL_TYPE)
     author = models.ForeignKey(User, on_delete=models.PROTECT)
-    status = models.IntegerField(choices=STATUS, default=0)
+    status = models.IntegerField(choices=STATUS, default=1)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
 
